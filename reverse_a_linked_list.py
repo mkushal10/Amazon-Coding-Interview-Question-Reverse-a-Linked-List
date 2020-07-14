5b7cdbb245f940e3a182041ac98f5eafd99a7e50
Kushal Neupane <mkushal10@gmail.com>
2020-07-14 11:10:44 +0545

    initial commit

--- reverse_a_linked_list.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def reverseList(self, head):
-        prev = None
-        while head:
-            temp = head
-            head = head.next
-            temp.next = prev
-            prev = temp
-        return prev
